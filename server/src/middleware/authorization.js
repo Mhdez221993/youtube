122665ae40076968f3b967d89bc1ccf35d2f2015
Moises <mhdezcoronado@gmail.com>
2021-09-21 16:22:48 +0200

    Create protect middleware

--- server/src/middleware/authorization.js
+++ server/src/middleware/authorization.js
@@ -25,16 +25,47 @@ export async function getAuthUser(req, res, next) {
   next();
 }
 
+// export async function protect(req, res, next) {
+//   if (!req.cookies.token) {
+//     return next({
+//       message: "You need to be logged in to visit this route",
+//       statusCode: 401,
+//     });
+//   }
+
+//   try {
+//     const token = req.cookies.token;
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+//     const user = await prisma.user.findUnique({
+//       where: {
+//         id: decoded.id,
+//       },
+//       include: {
+//         videos: true,
+//       },
+//     });
+
+//     req.user = user;
+//     next();
+//   } catch (error) {
+//     next({
+//       message: "You need to be logged in to visit this route",
+//       statusCode: 401,
+//     });
+//   }
+// }
+
 export async function protect(req, res, next) {
-  if (!req.cookies.token) {
+  if (!req.headers.authorization) {
     return next({
-      message: "You need to be logged in to visit this route",
+      message: "You need to be logged in to visit this routesss",
       statusCode: 401,
     });
   }
 
   try {
-    const token = req.cookies.token;
+    const token = req.headers.authorization;
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     const user = await prisma.user.findUnique({
