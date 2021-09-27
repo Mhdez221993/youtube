9c0b1d04cca7082101fbd7b2fc2c4bb46fa28998
Moises <mhdezcoronado@gmail.com>
2021-09-27 12:05:58 +0200

    Update authProvider context

--- client/src/context/auth-context.js
+++ client/src/context/auth-context.js
@@ -1,16 +1,30 @@
 import React from "react";
-import { useQuery } from "react-query";
+// import { useQuery } from "react-query";
 import { client } from "../utils/api-client";
 
 const AuthContext = React.createContext(null);
 
-export function AuthProvider({ children }) {
-  const { data } = useQuery("AuthProvider", () =>
-    client.get("/auth/me").then((res) => res.data.user)
-  );
-  const user = data || null;
+// export function AuthProvider({ children }) {
+//   const { data } = useQuery("AuthProvider", () =>
+//     client.get("/auth/me").then((res) => res.data.user)
+//   );
+//   const user = data || null;
 
-  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+//   return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+// }
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = React.useState(null)
+
+  React.useEffect(() => {
+    client.get("/auth/me").then(res => setUser(res.data.user))
+  }, [])
+
+  return (
+    <AuthContext.Provider value={user}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 export function useAuth() {
