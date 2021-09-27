8ac44b2fc73f2a81746874f7bc7305620975be50
Moises <mhdezcoronado@gmail.com>
2021-09-27 17:15:38 +0200

    Add preset from cloudDinary

--- client/src/components/UploadVideo.js
+++ client/src/components/UploadVideo.js
@@ -34,7 +34,7 @@ function UploadVideo() {
       const url = await uploadMedia({
         type: "video",
         file,
-        preset: "your-video-preset",
+        preset: "mw43eshy",
       });
 
       const extension = path.extname(url);
